0f60bd61b88f5bde09e2591be1b8eeecd3388c64
lpEdso <127642470+lpEdso@users.noreply.github.com>
2024-05-26 13:14:53 -0600

    Add files via upload

---
+++ BINNI LULAA/scripts.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+    setInterval(nextSlide, 6000); // Cambia de imagen cada 3 segundos
+});
+
+
+
